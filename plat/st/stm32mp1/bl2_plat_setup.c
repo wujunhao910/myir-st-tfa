7352dae0ab146fdbfc2810529336e577352d934c
Nicolas Toromanoff <nicolas.toromanoff@st.com>
2021-02-08 08:55:53 +0100

    feat(stm32mp1): manage potential tamper while boot
    In BL2, with BMEN=1 blocked secrets due to any potential tamper
    will be unblocked, and blocked again at end of boot when
    BMEN is reset to 0 at end of BL2.
    
    Change-Id: I27119ee11281a5991e799c9f5b7c4f6735b0c50b
    Signed-off-by: Nicolas Toromanoff <nicolas.toromanoff@st.com>

--- plat/st/stm32mp1/bl2_plat_setup.c
+++ plat/st/stm32mp1/bl2_plat_setup.c
@@ -299,6 +299,9 @@ void bl2_el3_plat_arch_setup(void)
 		goto skip_console_init;
 	}
 
+	/* Enter in boot mode */
+	stm32mp1_syscfg_boot_mode_enable();
+
 	stm32mp_print_cpuinfo();
 
 	board_model = dt_get_board_model();
@@ -579,4 +582,7 @@ void bl2_el3_plat_prepare_exit(void)
 	}
 
 	stm32mp1_security_setup();
+
+	/* end of boot mode */
+	stm32mp1_syscfg_boot_mode_disable();
 }
