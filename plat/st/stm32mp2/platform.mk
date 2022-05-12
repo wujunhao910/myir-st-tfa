b39b2a6f205e44159c0cd2da1332926b3e3c9f3c
Patrick Delaunay <patrick.delaunay@foss.st.com>
2022-05-12 19:43:28 +0200

    feat(stm32mp2): generate FIP for DDR initialization
    Generate a minimal FIP used for DDR initialization for serial boot
    when STM32MP_DDR_FIP_IO_STORAGE is activated.
    
    It is loaded in internal memory before to be used with support of
    the FIP memmap.
    
    Change-Id: I3a051ca2b258771e48c6e9fed9d77ab512c2416f
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/255409
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>

--- plat/st/stm32mp2/platform.mk
+++ plat/st/stm32mp2/platform.mk
@@ -238,4 +238,26 @@ check_ddr_type:
 		false; \
 	fi
 
+# generate separate DDR FIP image
+ifeq (${STM32MP_DDR_FIP_IO_STORAGE},1)
+ifneq ($(filter 1,${STM32MP_UART_PROGRAMMER} ${STM32MP_USB_PROGRAMMER}),)
+
+DDR_FIP_NAME		?=	fip-ddr.bin
+DDR_FIP_ARGS		+=	--ddr-fw ${STM32MP_DDR_FW}
+DDR_FIP_DEPS		+=	${STM32MP_DDR_FW}
+
+${BUILD_PLAT}/${DDR_FIP_NAME}: ${DDR_FIP_DEPS} fiptool
+	${Q}${FIPTOOL} create ${DDR_FIP_ARGS} $@
+	${Q}${FIPTOOL} info $@
+	@${ECHO_BLANK_LINE}
+	@echo "Built $@ successfully"
+	@${ECHO_BLANK_LINE}
+
+fip-ddr: ${BUILD_PLAT}/${DDR_FIP_NAME}
+
+fip: fip-ddr
+
+endif
+endif
+
 include plat/st/common/common_rules.mk
