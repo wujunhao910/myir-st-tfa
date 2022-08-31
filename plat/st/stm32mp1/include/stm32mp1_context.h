1f104a9d606003b441ca3178c8b77dda4bdda214
Yann Gautier <yann.gautier@foss.st.com>
2022-08-31 11:40:30 +0200

    feat(stm32mp13): save/restore DDRMCE config
    The DDRMCE regions configuration is saved in backup RAM context for
    standby mode. This allows reloading the configuration without fetching
    it from boot device.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: I29e5f2f16d6a9d08bdaac1d7cc0fd3443a92e3b1
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/264426
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Reviewed-by: Nicolas LE BAYON <nicolas.le.bayon@st.com>

--- plat/st/stm32mp1/include/stm32mp1_context.h
+++ plat/st/stm32mp1/include/stm32mp1_context.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2021, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2017-2022, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -10,6 +10,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include <drivers/st/stm32_mce.h>
+
 void stm32_clean_context(void);
 void stm32_context_save_bl2_param(void);
 uint32_t stm32_get_zdata_from_context(void);
@@ -19,6 +21,8 @@ uint32_t stm32_pm_get_optee_ep(void);
 #if STM32MP13
 void stm32mp1_pm_save_mce_mkey_in_context(uint8_t *data);
 void stm32mp1_pm_get_mce_mkey_from_context(uint8_t *data);
+void stm32mp1_pm_save_mce_region(uint32_t index, struct stm32_mce_region_s *config);
+void stm32mp1_pm_get_mce_region(uint32_t index, struct stm32_mce_region_s *config);
 #endif
 
 #endif /* STM32MP1_CONTEXT_H */
