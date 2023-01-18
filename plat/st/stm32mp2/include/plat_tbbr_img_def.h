a9bb370cc762a6591a18df0113c9716e77da43f9
Yann Gautier <yann.gautier@foss.st.com>
2023-01-18 14:37:52 +0100

    feat(stm32mp2): update image IDs
    To avoid taking too much place for authentication structures, reduce
    the MAX_NUMBER_IDS by rewriting some IDs over unused IDs.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: Ie835fba166ac35719316b48387eb623ee2bdc2c4
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/285745
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Domain-Review: Lionel DEBIEVE <lionel.debieve@foss.st.com>

--- plat/st/stm32mp2/include/plat_tbbr_img_def.h
+++ plat/st/stm32mp2/include/plat_tbbr_img_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, STMicroelectronics - All Rights Reserved
+ * Copyright (c) 2021-2023, STMicroelectronics - All Rights Reserved
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -9,14 +9,37 @@
 
 #include <export/common/tbbr/tbbr_img_def_exp.h>
 
-#if STM32MP_DDR_FIP_IO_STORAGE
-#define DDR_FW_ID		MAX_IMG_IDS_WITH_SPMDS
-#define MAX_IMG_WITH_DDR_IDS	MAX_IMG_IDS_WITH_SPMDS + 1
-#else
-#define MAX_IMG_WITH_DDR_IDS	MAX_IMG_IDS_WITH_SPMDS
-#endif
+/* Undef the existing values */
+#undef BKUP_FWU_METADATA_IMAGE_ID
+#undef FWU_METADATA_IMAGE_ID
+#undef FW_CONFIG_ID
+#undef ENC_IMAGE_ID
+#undef GPT_IMAGE_ID
+#undef NT_FW_CONFIG_ID
+#undef SOC_FW_CONFIG_ID
+#undef TB_FW_CONFIG_ID
 
-#define MAX_NUMBER_IDS		MAX_IMG_WITH_DDR_IDS
+/* Define the STM32MP1 used ID */
+#define BKUP_FWU_METADATA_IMAGE_ID	U(1)
+#define FWU_METADATA_IMAGE_ID		U(2)
+#define FW_CONFIG_ID			U(8)
+#define ENC_IMAGE_ID			U(12)
+#define GPT_IMAGE_ID			U(16)
+#define NT_FW_CONFIG_ID			U(18)
+#define SOC_FW_CONFIG_ID		U(19)
+#define TB_FW_CONFIG_ID			U(20)
+#define STM32MP_CONFIG_CERT_ID		U(24)
+
+#if STM32MP_DDR_FIP_IO_STORAGE
+#define DDR_FW_ID			U(27)
+/* Increase the MAX_NUMBER_IDS to match the authentication pool required */
+#define MAX_NUMBER_IDS			U(28)
+
+#else
+/* Increase the MAX_NUMBER_IDS to match the authentication pool required */
+#define MAX_NUMBER_IDS			U(27)
+
+#endif
 
 #endif /* PLAT_TBBR_IMG_DEF_H */
 
