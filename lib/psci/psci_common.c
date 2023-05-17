6a19eb0bdb33031bdb2a458450b8a918b93f4334
Boyan Karatotev <boyan.karatotev@arm.com>
2023-05-17 12:20:09 +0100

    refactor(psci): extract cm_prepare_el3_exit_ns() to a common location
    PSCI on and suspend wakeup both end with a cm_prepare_el3_exit_ns() call.
    Since they are equivalent to the caller, move the call to just after the
    *_finish calls to deduplicate it.
    
    [Backport of commit e07e7392a1ee ("refactor(psci): extract
     cm_prepare_el3_exit_ns() to a common location")]
    
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>
    Change-Id: I05c16dc6613aba357d20cc39cc43aab803d675e0
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/347271
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    Domain-Review: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    Tested-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@foss.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- lib/psci/psci_common.c
+++ lib/psci/psci_common.c
@@ -1020,6 +1020,13 @@ void psci_warmboot_entrypoint(void)
 		psci_cpu_suspend_finish(cpu_idx, &state_info);
 
 	/*
+	 * Generic management: Now we just need to retrieve the
+	 * information that we had stashed away during the cpu_on
+	 * call to set this cpu on its way.
+	 */
+	cm_prepare_el3_exit_ns();
+
+	/*
 	 * Set the requested and target state of this CPU and all the higher
 	 * power domains which are ancestors of this CPU to run.
 	 */
