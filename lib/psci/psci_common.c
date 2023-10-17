4ed70e3deb3e07c60266b97dd7312f4cf318db45
Patrick Delaunay <patrick.delaunay@foss.st.com>
2023-10-17 20:17:58 +0200

    fix(psci): fix parent parsing in psci_is_last_cpu_to_idle_at_pwrlvl
    The current loop has no effect as it uses a fixed parameter = my_idx
    and returns the FIRST parent of CPU.
    
    This update allows to parse the PSCI topology tree to find
    the parent at parent level of the CPU identified by my_idx.
    
    Fixes: 110569d745db ("feat(psci): add support for OS-initiated mode")
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Change-Id: Icd7da0bc58200b307c80e18d8d3a9302ac25365b
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/334407
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>

--- lib/psci/psci_common.c
+++ lib/psci/psci_common.c
@@ -179,9 +179,10 @@ static bool psci_is_last_cpu_to_idle_at_pwrlvl(unsigned int end_pwrlvl)
 	}
 
 	my_idx = plat_my_core_pos();
+	parent_idx = my_idx;
 
 	for (lvl = PSCI_CPU_PWR_LVL; lvl <= end_pwrlvl; lvl++) {
-		parent_idx = psci_cpu_pd_nodes[my_idx].parent_node;
+		parent_idx = psci_cpu_pd_nodes[parent_idx].parent_node;
 	}
 
 	cpu_start_idx = psci_non_cpu_pd_nodes[parent_idx].cpu_start_idx;
