6f1d5115203327ecdcf9431ad61562a63338c268
Yann Gautier <yann.gautier@foss.st.com>
2023-11-29 13:53:42 +0100

    feat(bl): align DATA section end on 2 bytes
    NOT_UPSTREAMABLE
    
    This is a workaround for Hyperflash boot on RevA and RevB revisions of
    STM32MP25.
    
    Change-Id: I3242fb8fe593904616e0d8bb0b889906f625ce87
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/342677
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>

--- include/common/bl_common.ld.h
+++ include/common/bl_common.ld.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2022, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2023, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -111,6 +111,7 @@
 	.data . : ALIGN(DATA_ALIGN) {			\
 		__DATA_START__ = .;			\
 		*(SORT_BY_ALIGNMENT(.data*))		\
+		. = ALIGN(2);				\
 		__DATA_END__ = .;			\
 	}
 
