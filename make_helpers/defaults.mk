04715303736b1ca6475ea120f3c55e0d215b2673
Yann Gautier <yann.gautier@foss.st.com>
2024-04-05 11:06:33 +0200

    feat(fwu): add a config flag for including image info in the FWU metadata
    The version 2 of the FWU metadata structure is designed such that the
    information on the updatable images can be omitted from the metadata
    structure. Add a configuration flag, PSA_FWU_METADATA_FW_STORE_DESC,
    which is used to select whether the metadata structure has this
    information included or not. It's value is set to 1 by default.
    
    Change-Id: I4463a20c94d2c745ddb0b2cc8932c12d418fbd42
    Signed-off-by: Sughosh Ganu <sughosh.ganu@linaro.org>
    (cherry picked from commit 11d05a77295885f27530cf07029ebc2b36f49918)
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/371622
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Maxime MERE <maxime.mere@foss.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
    Tested-by: Yann GAUTIER <yann.gautier@foss.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>

--- make_helpers/defaults.mk
+++ make_helpers/defaults.mk
@@ -456,6 +456,14 @@ NR_OF_IMAGES_IN_FW_BANK		:= 1
 # Disable Firmware update support by default
 PSA_FWU_SUPPORT			:= 0
 
+# Enable image description in FWU metadata by default when PSA_FWU_SUPPORT
+# is enabled.
+ifeq ($(PSA_FWU_SUPPORT),1)
+PSA_FWU_METADATA_FW_STORE_DESC	:= 1
+else
+PSA_FWU_METADATA_FW_STORE_DESC	:= 0
+endif
+
 # By default, disable access of trace buffer control registers from NS
 # lower ELs  i.e. NS-EL2, or NS-EL1 if NS-EL2 implemented but unused
 # if FEAT_TRBE is implemented.
